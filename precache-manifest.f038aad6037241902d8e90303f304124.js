43ec94792dfab01834ee46a033b96e1528e28c3b
Nick <b10303008@gmail.com>
2020-05-01 01:16:48 +0800

    Updates

---
+++ precache-manifest.f038aad6037241902d8e90303f304124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edaccf9c0655e866e8af779f28e0d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "856efb94af17da3d9977",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca6ed69dd1ba36f0109c",
+    "url": "/static/js/2.23b69d7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23b69d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856efb94af17da3d9977",
+    "url": "/static/js/main.8ee20e20.chunk.js"
+  },
+  {
+    "revision": "45ddb844310470fd851e",
+    "url": "/static/js/runtime-main.a171bfb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
